fa0ad5253cb52c7866290623a88f02bd38b80fac
Hermine Grigoryan <hermine_grigoryan@edu.aua.am>
2019-03-30 21:42:26 +0400

    fixed number of pages

--- new_scraping.R
+++ new_scraping.R
@@ -6,9 +6,6 @@ URL <- "https://www.tripadvisor.com/Hotels-g293931-Armenia-Hotels.html"
 (LINKS<-read_html(URL) %>% 
   html_nodes(css="a.linkText") %>% html_attr("href"))
 LINKS<-paste("https://www.tripadvisor.com/", LINKS, sep="")
-read_html(LINKS[1])
-i<-1
-rm(i)
 
 num_pages<-c()
 
@@ -16,10 +13,10 @@ for(i in 1:length(LINKS)){
   HTML<-read_html(LINKS[i])
   number_of_pages<-HTML %>% html_nodes(css=".pageNum") %>% html_text()
   number_of_pages<-as.numeric(rev(number_of_pages)[1])
-  link_num_pages<-data.frame(HTML, number_of_pages)
+  link_num_pages<-c(LINKS[i], number_of_pages)
   num_pages<-rbind(num_pages, link_num_pages)
-  print(number_of_pages)
 }
 
-
-
+num_pages<-data.frame(num_pages)
+colnames(num_pages)<-c("Link", "N_Pages")
+rownames(num_pages)<-NULL
