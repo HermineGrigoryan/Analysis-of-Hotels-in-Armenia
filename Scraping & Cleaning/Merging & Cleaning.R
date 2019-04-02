75183b98c84062709d50c62ed1ef304d1a1d041d
Hermine Grigoryan <hermine_grigoryan@edu.aua.am>
2019-04-02 23:39:23 +0400

    finished merging and cleaning

--- Scraping & Cleaning/Merging & Cleaning.R
+++ Scraping & Cleaning/Merging & Cleaning.R
@@ -34,4 +34,21 @@ scoreDistribution<-apply(scoreDistribution, 2, as.numeric)
 scoreDistribution[is.na(scoreDistribution)]<-0
 
 merged_data<-data.frame(merged_data, scoreDistribution)
+dat<-merged_data
 
+final_data<-data.frame(Name=dat$name, Link=dat$link, Price_AMD=dat$Price_AMD, Price_USD=dat$Price_USD,
+                       State=dat$state, City=dat$city, Address=dat$address, Zipcode=dat$zipcode, 
+                       Photos_Count=dat$photosCount, Ranking_Category=dat$rankingCategory, 
+                       Rank_in_Category=dat$objectRank, N_Hotels_in_Category=dat$objectsInCategoryCount, 
+                       Review_Count=dat$Review_Count, Average_Score=dat$avgScore, Excellent_Rating=dat$Excellent, 
+                       Good_Rating=dat$Good, Average_Rating=dat$Average, Poor_Rating=dat$Poor, Terrible_Rating=dat$Terrible,  
+                       Certificate=dat$certificate, Language_distribution=dat$languageDistribution, 
+                       Provider=dat$Provider, Free_WiFi=dat$Free_Wifi,Breakfast_Included=dat$Breakfast_Included,
+                       Free_Parking=dat$Free_Parking,
+                       Swimming_Pool=dat$Swimming_Pool, Hotel_Website=dat$Hotel_Website, Special_Offer=dat$Special_Offer,
+                       Restaurant=dat$Restaurant)
+
+final_data$Certificate<-ifelse(final_data$Certificate==FALSE, 0, 1)
+str(final_data)
+
+#write.csv(final_data, "Trip_Advisor_Armenian_Hotels.csv", row.names = F)
